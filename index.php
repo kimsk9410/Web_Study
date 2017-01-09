c545c644a710e2296203c7886a63a429c2af2e1c
seonkyu kim <seonkyu kim>
2017-01-09 14:14:57 +0900

    SW-Track

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-ㅇㅇ
+<?php
+include "header.php";
+?>
+<body>
+    <div id="map"></div>
+    <script>
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfinta_lc8A__cwgyfe-YUHefgdMeNgOI&callback=initMap"
+        async defer></script>
+  </body>
+<?php
+include "footer.php";
+?>
